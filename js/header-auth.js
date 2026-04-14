4f83c45cd988d467412c75503cb6278c8278bce5
BazarMarket <alexey_cy@hotmail.com>
2026-04-14 20:10:21 +0000

    Simplify header-auth.js avatar: CSS handles shape, just set img.src

--- js/header-auth.js
+++ js/header-auth.js
@@ -18,13 +18,7 @@
             var img = avatarEl.querySelector('img') || (avatarEl.tagName === 'IMG' ? avatarEl : null);
             if (img) {
                 var savedAv = localStorage.getItem('bazar_avatar');
-                if (savedAv) {
-                    img.src = savedAv;
-                    img.style.cssText = 'display:block;width:100%;height:100%;object-fit:cover;border-radius:50%;';
-                } else {
-                    img.src = gender === 'female' ? 'icon/woman.png' : 'icon/man.svg';
-                    img.style.cssText = '';
-                }
+                img.src = savedAv || (gender === 'female' ? 'icon/woman.png' : 'icon/man.svg');
             }
         }
     }
