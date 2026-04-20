2545318795bb3398f643c50107cfc96898649b74
BazarMarket <alexey_cy@hotmail.com>
2026-04-20 00:23:58 +0000

    fix dev.sh: sync to /var/www/bazar-dev/public/ (correct nginx root)

--- dev.sh
+++ dev.sh
@@ -6,10 +6,10 @@ if [ ! -f ~/.ssh/bazar_deploy ]; then
 fi
 echo "=== Saving Production → Dev ==="
 ssh -i .local/ssh/bazar_deploy -o StrictHostKeyChecking=no root@49.13.231.137 "
-  rsync -av /var/www/bazar-prod/public/*.html /var/www/bazar-dev/ &&
-  rsync -av /var/www/bazar-prod/public/css/   /var/www/bazar-dev/css/ &&
-  rsync -av /var/www/bazar-prod/public/js/    /var/www/bazar-dev/js/ &&
-  rsync -av /var/www/bazar-prod/public/img/   /var/www/bazar-dev/img/ &&
-  rsync -av /var/www/bazar-prod/public/icon/  /var/www/bazar-dev/icon/
+  rsync -av /var/www/bazar-prod/public/*.html /var/www/bazar-dev/public/ &&
+  rsync -av /var/www/bazar-prod/public/css/   /var/www/bazar-dev/public/css/ &&
+  rsync -av /var/www/bazar-prod/public/js/    /var/www/bazar-dev/public/js/ &&
+  rsync -av /var/www/bazar-prod/public/img/   /var/www/bazar-dev/public/img/ &&
+  rsync -av /var/www/bazar-prod/public/icon/  /var/www/bazar-dev/public/icon/
 "
 echo "=== Done! Dev is now a copy of Production ==="
